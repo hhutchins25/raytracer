112b07cbbca84394fd2cc2640724a5461aa66f7d
holden <hhutchins25@gmail.com>
2018-12-30 01:23:49 -0800

    8 - Mode selection
    Added mode selection to the HTML, will be adding more properties to change and possible reflectivity

--- raytracer_fn.js
+++ raytracer_fn.js
@@ -2,7 +2,9 @@
 var worldObjects = [];
 var lightObjects = [];
 var canvas = document.getElementById("raytracerCanvas");
+var ctx = canvas.getContext("2d");
 
+// Functions for instatiating objects/intersections
 function Intersection(obj, pos) {
 	this.obj = obj;
 	this.pos = pos;
@@ -21,6 +23,7 @@ function Sphere(posVec, radius, color) {
 	worldObjects.push(this);
 }
 
+// Functions for creating lights
 function Light(posVec, color, intensity) {
 	WorldObject.call(this, posVec, color);
 	this.intensity = intensity;
@@ -34,9 +37,10 @@ function AmbientLight(posVec, color, intensity) {
 function DirectionalLight(posVec, color, intensity, dirVec) {
 	Light.call(this, posVec, color, intensity);
 	this.dirVec = dirVec;
-	lightObjects.push(this); 
+	lightObjects.push(this);
 }
 
+// Basic vector math
 function dotProd(vec1, vec2) {
 	return vec1.reduce((sum, val, idx) => {
 		return sum + (val * vec2[idx]);
@@ -61,12 +65,12 @@ function rayTracer(width, height, fov, near, far, mode) {
 	let yPos = [];
 	// Per-pixel pos/vector horizontally
 	for (let i = 1; i <= width; i++) {
-		let currFov = (fovInc *  i) - (fov / 2);
+		let currFov = (fovInc * i) - (fov / 2);
 		let htan = Math.tan(currFov * (Math.PI / 180));
 		xPos.push(htan * near);
 	// Per-pixel pos/vector vertically
 	} for (let i = 1; i <= height; i++) {
-		let currVFov = (vfovInc *  i) - (vfov / 2);
+		let currVFov = (vfovInc * i) - (vfov / 2);
 		let vtan = Math.tan(currVFov * (Math.PI / 180));
 		yPos.push(vtan * near);
 	}
@@ -113,7 +117,7 @@ function rayCheck(dirVec) {
 			} else if (quad2 > 0) {
 				allIntrscts.push(new Intersection(obj, point2));
 			}
-		} else if (!Number.isNaN(quad1) && quad1 > 0) { 
+		} else if (!Number.isNaN(quad1) && quad1 > 0) {
 			allIntrscts.push(new Intersection(obj, point1));
 		} else if (!Number.isNaN(quad2) && quad2 > 0) {
 			allIntrscts.push(new Intersection(obj, point2));
@@ -129,7 +133,7 @@ function closestToOrigin(intersections) {
 	let minIntrsct = new Intersection(null, null);
 	intersections.forEach((intrsct) => {
 		let pointDist = Math.pow(intrsct.obj.x, 2) + Math.pow(intrsct.obj.y, 2) + Math.pow(intrsct.obj.z, 2);
-		if (pointDist < minVal) { 
+		if (pointDist < minVal) {
 			minVal = pointDist;
 			minIntrsct = intrsct;
 		}
@@ -141,18 +145,18 @@ function closestToOrigin(intersections) {
 // on the informations gathered and given
 function drawPixel(pos, mode, obj, intrsctPos, camRay) {
 	let color;
-	let ctx = canvas.getContext("2d");
+	// Clear canvas before drawing
 	if (obj === null || intrsctPos === null) {
 		color = "#000000";
 	} else if (mode === "bool") {
 		color = "#FFFFFF";
 	} else if (mode === "color") {
 		color = rgb2hex(obj.color);
-	} else if (mode === "lighting") {
+	} else if (mode === "lighting" || mode === "spec") {
 		let mult = 0;
 		lightObjects.forEach((light) => {
 			if (light instanceof DirectionalLight) {
-				mult += lightPixel(light.dirVec, light.intensity, obj, intrsctPos, camRay);
+				mult += lightPixel(light.dirVec, light.intensity, obj, intrsctPos, camRay, mode);
 			} else {
 				mult += light.intensity;
 			}
@@ -163,47 +167,70 @@ function drawPixel(pos, mode, obj, intrsctPos, camRay) {
 	ctx.fillRect(pos[0], pos[1], 1, 1);
 }
 
-function rgb2hex (rgb) { 
+function rgb2hex(rgb) {
 	str = "#";
 	rgb.forEach((val) => {
 		num = Math.round(val);
 		currHex = Number(num).toString(16);
 		if (currHex.length < 2) {
 			currHex = "0" + currHex;
-	   	}
+		}
 		str += currHex;
 	});
 	return (str);
 }
 
-function lightPixel(lightDir, intensity, obj, intrsctPos, camRay) {
+// Determines light intensity per pixel
+function lightPixel(lightDir, intensity, obj, intrsctPos, camRay, mode) {
+	// Calculate light intensity
 	let objPos = [obj.x, obj.y, obj.z];
 	let objNormVec = intrsctPos.map((val, idx) => (val - objPos[idx]) / obj.radius);
 	let unitCamRay = normalVec(camRay);
 	let lightMag = dotProd(objNormVec, lightDir);
-	let incidVec = lightDir.map(val => val * -1);
-	let incidNormDot = dotProd(incidVec, objNormVec);
-	let reflVec = [0,0,0];
-	incidVec.forEach((val, idx) => {
-		reflVec[idx] = val - (2 * objNormVec[idx] * incidNormDot); 
-	});
-	let specMult = Math.max(0, dotProd(reflVec, unitCamRay));
+	// Calculate specular intensity
+	let specMult = 0;
+	if (mode === "spec") {
+		let incidVec = lightDir.map(val => val * -1);
+		let incidNormDot = dotProd(incidVec, objNormVec);
+		let reflVec = [0, 0, 0];
+		incidVec.forEach((val, idx) => {
+			reflVec[idx] = val - (2 * objNormVec[idx] * incidNormDot);
+		});
+		specMult = Math.max(0, dotProd(reflVec, unitCamRay));
+	}
+	// Return an intensity scalar combining lighting and specular
 	if (lightMag < 0) {
 		return 0;
 	} else {
-	return (lightMag * intensity + Math.pow(specMult, 18));
+		return (lightMag * intensity + Math.pow(specMult, 64));
 	}
 }
 
 // Dev testing values
-var vec = [0, 0, 25000];
-var sphere1 = new Sphere(vec, 1000, [255,0,0]);
-var vec = [2000, 2800, 15000];
-var sphere2 = new Sphere(vec, 1000, [255,255,0]);
-var vec = [-2000, -600, 10000];
-var sphere3 = new Sphere(vec, 1000, [0,255,255]);
-var vec = [1000, -300, 5000];
-var sphere4 = new Sphere(vec, 1000, [150,240,20]);
-var dirLight1 = new DirectionalLight([0,0,0], [0,0,0], 0.7, [0.707,0.707,0]);
-var ambLight1 = new AmbientLight([0,0,0], [0,0,0], 0.2);
-rayTracer(canvas.width, canvas.height, 30, 1000, 100000, "lighting");
+function initRaytracer() {
+	// Delete all previous objects
+	worldObjects = [];
+	lightObjects = [];
+	// Clear the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	// Instantiate all objects
+	var vec = [0, 0, 25000];
+	var sphere1 = new Sphere(vec, 1000, [255, 0, 0]);
+	var vec = [2000, 2800, 15000];
+	var sphere2 = new Sphere(vec, 1000, [255, 255, 0]);
+	var vec = [-2000, -600, 10000];
+	var sphere3 = new Sphere(vec, 1000, [0, 255, 255]);
+	var vec = [1000, -300, 5000];
+	var sphere4 = new Sphere(vec, 1000, [150, 240, 20]);
+	var dirLight1 = new DirectionalLight([0, 0, 0], [0, 0, 0], 0.4, [0.707, 0.707, 0]);
+	var dirVecLight = normalVec([-1, 1, 1]);
+	var dirLight2 = new DirectionalLight([0, 0, 0], [0, 0, 0], 0.2, dirVecLight);
+	var ambLight1 = new AmbientLight([0, 0, 0], [0, 0, 0], 0.2);
+	var modeSelect = document.getElementById("modeSelect");
+	var mode = modeSelect.options[modeSelect.selectedIndex].value;
+	console.log(mode);
+	// Initialize raytracing process
+	rayTracer(canvas.width, canvas.height, 30, 1000, 100000, mode);
+}
+
+initRaytracer();
